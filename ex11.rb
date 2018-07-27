7ffd9c9fd47172ee568a00a17433c97d8b6de44f
Prabhu161292 <41663693+Prabhu161292@users.noreply.github.com>
2018-07-27 13:30:32 +0530

    Exercise 11

---
+++ ex11.rb
@@ -0,0 +1,7 @@
+puts "How old are you"
+age=gets.chomp
+puts "How tall are you"
+height=gets.chomp
+puts "How much do you weigh"
+weight=gets.chomp
+print "So you're #{age} old,#{height}cm tall and #{weight}kgs heavy.\n"
